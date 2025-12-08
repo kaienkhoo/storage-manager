86a45f4b4e72ad189676760346a24e5e03fa2d22
kaienkhoo <100989619+kaienkhoo@users.noreply.github.com>
2025-12-08 21:14:10 +0800

    Checkpoint

---
+++ storage-manager/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
---
+++ storage-manager/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { Sign } from 'crypto'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SignIn
